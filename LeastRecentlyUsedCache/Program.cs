31dc1c2caf5b1149628ce89355eea8d63c80847f
Olga Nosova <semverst@yandex.ru>
2020-11-30 23:09:30 +0300

    Add project files.

---
+++ LeastRecentlyUsedCache/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeastRecentlyUsedCache
+{
+    /// <summary>
+    /// https://leetcode.com/problems/lru-cache/
+    /// НЕ поняла из описания, как должно быть. Моя реализация не прошла тесты, Чужая прошла.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lRUCache = new LRUCache2(2);
+            lRUCache.Put(1, 1); // cache is {1=1}
+            Console.WriteLine("null");
+
+            lRUCache.Put(2, 2); // cache is {1=1, 2=2}
+            Console.WriteLine("null");
+
+            lRUCache.Get(1);    // return 1
+            Console.WriteLine(lRUCache.Get(1));
+
+            lRUCache.Put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+            Console.WriteLine("null");
+
+            lRUCache.Get(2);    // returns -1 (not found)
+            Console.WriteLine(lRUCache.Get(2));
+
+            lRUCache.Put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+            Console.WriteLine("null");
+
+            lRUCache.Get(9);    // return -1 (not found)
+            Console.WriteLine(lRUCache.Get(9));
+
+            lRUCache.Get(3);    // return 3
+            Console.WriteLine(lRUCache.Get(3));
+
+            lRUCache.Get(4);    // return 4
+            Console.WriteLine(lRUCache.Get(4));
+
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// Моя реализация
+    /// </summary>
+    public class LRUCache
+    {
+
+        public LRUCache(int capacity)
+        {
+            _capacity = capacity;
+            _cache = new Dictionary<int, int>();
+            _currentlyUsed = new Stack<int>();
+        }
+
+        public int Get(int key)
+        {
+            if (key < 0 || key > 3000)
+                return _notFound;
+
+            if (_cache.ContainsKey(key))
+                return _cache[key];
+
+            return _notFound;
+        }
+
+        public void Put(int key, int value)
+        {
+            if (_capacity == _cache.Count)
+            {
+                _cache.Remove(_currentlyUsed.Pop());
+                _cache[key] = value;
+                return;
+            }
+
+            if (!_cache.ContainsKey(key))
+                _currentlyUsed.Push(key);
+
+            _cache[key] = value;
+        }
+
+        private int _capacity;
+        private Stack<int> _currentlyUsed;
+        private Dictionary<int, int> _cache;
+
+        private int _notFound = -1;
+    }
+
+    /// <summary>
+    /// Чужая реализация, медленная
+    /// </summary>
+    public class LRUCache2
+    {
+
+        private int capacity;
+        private Dictionary<int, int> dic;
+        private LinkedList<int> cacheList;
+
+        public LRUCache2(int capacity)
+        {
+            this.capacity = capacity;
+            dic = new Dictionary<int, int>();
+            cacheList = new LinkedList<int>();
+        }
+
+        public int Get(int key)
+        {
+            if (!dic.ContainsKey(key))
+                return -1;
+
+            cacheList.Remove(key);
+            cacheList.AddFirst(key);
+            return dic[key];
+        }
+
+        public void Put(int key, int value)
+        {
+            if (dic.ContainsKey(key))
+            {
+                // update the value of cache
+                dic[key] = value;
+
+                // re-arrange the order of cacheList
+                cacheList.Remove(key);
+                cacheList.AddFirst(key);
+            }
+            else
+            {
+                if (dic.Count >= capacity)
+                {
+                    dic.Remove(cacheList.Last.Value);
+                    cacheList.RemoveLast();
+                }
+
+                // add cache
+                dic.Add(key, value);
+                cacheList.AddFirst(key);
+            }
+        }
+    }
+}
---
+++ Number-of-Operations-to-Make-Network-Connected/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+
+namespace Number_of_Operations_to_Make_Network_Connected
+{
+    /// <summary>
+    /// https://leetcode.com/problems/number-of-operations-to-make-network-connected/
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            // Input: n = 4, connections = [[0,1],[0,2],[1,2]]
+            // Output: 1
+            Console.WriteLine(solution.MakeConnected(4, new int[][]
+            {
+                new []{0,1},
+                new []{0,2},
+                new []{1,2}
+            }));
+            Console.WriteLine();
+
+            // Input: n = 6, connections = [[0,1],[0,2],[0,3],[1,2],[1,3]]
+            // Output: 2
+            Console.WriteLine(solution.MakeConnected(6, new int[][]
+            {
+                new []{0,1},
+                new []{0,2},
+                new []{0,3},
+                new []{1,2},
+                new []{1,3}
+            }));
+            Console.WriteLine();
+
+            // Input: n = 6, connections = [[0,1],[0,2],[0,3],[1,2]]
+            // Output: -1
+            Console.WriteLine(solution.MakeConnected(6, new int[][]
+            {
+                new []{0,1},
+                new []{0,2},
+                new []{0,3},
+                new []{1,2}
+            }));
+            Console.WriteLine();
+
+            // Input: n = 5, connections = [[0,1],[0,2],[3,4],[2,3]]
+            // Output: 0
+            Console.WriteLine(solution.MakeConnected(5, new int[][]
+            {
+                new []{0,1},
+                new []{0,2},
+                new []{3,4},
+                new []{2,3}
+            }));
+            Console.WriteLine();
+        }
+    }
+
+    public class Solution
+    {
+        public int MakeConnected(int n, int[][] connections)
+        {
+            if (n <= 0)
+                throw new ArgumentOutOfRangeException();
+
+            // todo: check, id int[i].Legnth > 2 and others
+
+            int extraConnections = 0;
+            int root = 0;
+            var union = new Union(n);
+            for (var i = 0; i < connections.Length; i++)
+            {
+                if (union.connected(connections[i][0], connections[i][1]))
+                    extraConnections++;
+                else
+                {
+                    root = union.union(connections[i][0], connections[i][1]); ;
+                }
+            }
+
+            // iteration via ids
+            var extraConnectionsCounter = 0;
+            for (var i = 0; i < n; i++)
+            {
+                if (!union.connected(i, root))
+                {
+                    root = union.union(i, root);
+                    extraConnectionsCounter++;
+                    extraConnections--;
+                    if (extraConnections < 0)
+                        return -1;
+                }
+            }
+            return extraConnections < 0 ? -1 : extraConnectionsCounter;
+        }
+    }
+
+    public class Union
+    {
+        private int[] ids { get; set; }
+        private int size;
+
+        public Union(int n)
+        {
+            if (n <= 0)
+                throw new ArgumentOutOfRangeException();
+
+            size = n;
+            ids = new int[n];
+            for (var i = 0; i < n; i++)
+            {
+                ids[i] = i;
+            }
+        }
+
+        public bool connected(int p, int q)
+        {
+            if (p >= size || q >= size)
+                throw new ArgumentOutOfRangeException();
+
+            return ids[p] == ids[q];
+        }
+
+        /// <summary>
+        /// return current root
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="q"></param>
+        /// <returns></returns>
+        public int union(int p, int q)
+        {
+            if (p >= size || q >= size)
+                throw new ArgumentOutOfRangeException();
+
+            var rootP = ids[p];
+            var rootQ = ids[q];
+            for (var i = 0; i < size; i++)
+            {
+                if (ids[i] == rootP || ids[i] == rootQ)
+                    ids[i] = rootQ;
+            }
+
+            return rootQ;
+        }
+    }
+}
