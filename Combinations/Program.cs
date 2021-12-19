4d731476305734c09e5437f13ef4336866005ae1
Olga Nosova <semverst@yandex.ru>
2021-12-19 17:16:53 +0300

    Permutaions and combinations classics algorithm

---
+++ Combinations/Program.cs
@@ -0,0 +1,84 @@
+﻿// https://leetcode.com/problems/combinations
+
+var solution = new Solution();
+
+// Input: n = 4, k = 2
+//    Output:
+//    [
+//      [2,4],
+//      [3,4],
+//      [2,3],
+//      [1,2],
+//      [1,3],
+//      [1,4],
+//    ]
+Console.WriteLine(solution.Combine(4, 2));
+
+/// <summary>
+/// My solution from memory
+/// </summary>
+public class Solution
+{
+    private int k;
+    private int n;
+    private IList<IList<int>> output = new List<IList<int>>();
+
+    public IList<IList<int>> Combine(int n, int k)
+    {
+        this.k = k;
+        this.n = n;
+
+        Backtrack(1, new Stack<int>());
+        return output;
+    }
+
+    private void Backtrack(int first, Stack<int> current)
+    {
+        if (current.Count == k)
+            output.Add(current.ToList());
+
+        for (var i = first; i <= n; i++)
+        {
+            current.Push(i);
+            Backtrack(i + 1, current);
+            current.Pop();
+        }
+    }
+}
+
+/// <summary>
+/// Leetcode's solution
+/// </summary>
+class Solution2
+{
+    private IList<IList<int>> output = new List<IList<int>>();
+    int n;
+    int k;
+
+    private void Backtrack(int first, List<int> curr)
+    {
+        // if the combination is done
+        if (curr.Count == k)
+            output.Add(curr.ToList());
+        for (int i = first; i < n + 1; ++i)
+        {
+            // add i into the current combination
+            curr.Add(i);
+            // use next integers to complete the combination
+            Backtrack(i + 1, curr);
+            // backtrack
+            curr.RemoveAt(curr.Count - 1);
+        }
+    }
+
+    public IList<IList<int>> Combine(int n, int k)
+    {
+
+        this.n = n;
+        this.k = k;
+
+        Backtrack(1, new List<int>());
+        return output;
+    }
+}
+
---
+++ Permutations/Program.cs
@@ -0,0 +1,79 @@
+﻿// https://leetcode.com/problems/permutations/
+
+var solution = new Solution();
+
+// Input: nums = [1, 2, 3]
+// Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+Console.WriteLine(solution.Permute(new int[] { 1, 2, 3 }));
+
+/// <summary>
+/// My solution from memory
+/// </summary>
+public class Solution {
+    public IList<IList<int>> Permute(int[] nums) {
+        var output = new List<IList<int>>();
+        backtrack(0, nums, nums.Length, output);
+        return output;
+    }
+    
+    private void backtrack(int first, int[] nums, int length, IList<IList<int>> output) 
+    {
+        if(first == length) 
+            output.Add(nums.ToList());
+        
+        for(var i = first; i < length; i++)
+        {
+            Swap(nums, first, i);
+            backtrack(first + 1, nums, length, output);
+            Swap(nums, first, i);
+        }
+    }
+    
+    private void Swap(int[] nums, int index1, int index2){
+        var temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;            
+    }
+}
+
+/// <summary>
+/// Leetcode's solution
+/// </summary>
+class Solution2
+{
+    private void Backtrack(int n,
+                         int[] nums,
+                         IList<IList<int>> output,
+                         int first)
+    {
+        // if all integers are used up
+        if (first == n)
+            output.Add(nums.ToList());
+        for (int i = first; i < n; i++)
+        {
+            // place i-th integer first 
+            // in the current permutation
+            Swap(nums, first, i);
+            // use next integers to complete the permutations
+            Backtrack(n, nums, output, first + 1);
+            // backtrack
+            Swap(nums, first, i);
+        }
+    }
+
+    public IList<IList<int>> Permute(int[] nums)
+    {
+        // init output list
+        IList<IList<int>> output = new List<IList<int>>();
+
+        Backtrack(nums.Length, nums, output, 0);
+        return output;
+    }
+
+    private static void Swap(int[] array, int index1, int index2)
+    {
+        var temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+}
